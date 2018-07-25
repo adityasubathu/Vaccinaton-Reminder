db1e8c4d790834683dd54d687a21c3bf707636c3
Aditya <adi7051@gmail.com>
2018-07-25 12:30:26 +0530

    added a todo to check alarm

--- app/src/main/java/org/vaccinationreminder/AlarmReceiver.java
+++ app/src/main/java/org/vaccinationreminder/AlarmReceiver.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
+
+    // todo: check why this isn't working
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "An Alarm Has Been Triggered", Toast.LENGTH_SHORT).show();
 
