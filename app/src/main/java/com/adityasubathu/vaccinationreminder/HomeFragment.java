e81b09bb79e778dfc9682410a21aaed686ffcc9e
Aditya <adi7051@gmail.com>
2018-08-17 13:53:20 +0530

    added Alert Dialog & replaced toast with snack bar when deleting a child

--- app/src/main/java/com/adityasubathu/vaccinationreminder/HomeFragment.java
+++ app/src/main/java/com/adityasubathu/vaccinationreminder/HomeFragment.java
@@ -2,12 +2,15 @@ package com.adityasubathu.vaccinationreminder;
 
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,7 +22,6 @@ import android.widget.AdapterView;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -68,16 +70,43 @@ public class HomeFragment extends Fragment {
 
         lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                String deletedName = (String) adap.getGroup(position);
-                Toast.makeText(getActivity(), childrenList.get(position) + " deleted", Toast.LENGTH_SHORT).show();
+            public boolean onItemLongClick(AdapterView<?> parent, final View view, final int position, long id) {
 
-                helper.delete(deletedName);
-                adap.childrenList.remove(position);
-                adap.DOBList.remove(position);
-                adap.nextDateList.remove(position);
-                adap.notifyDataSetChanged();
-                alarm.cancelAlarm(getActivity(), position);
+                final String deletedName = (String) adap.getGroup(position);
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.VaccineReminderMaterialDialogTheme);
+
+                builder.setTitle("Delete " + deletedName);
+                builder.setMessage("Are you sure you want to delete " + deletedName + "?");
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        helper.delete(deletedName);
+                        adap.childrenList.remove(position);
+                        adap.DOBList.remove(position);
+                        adap.nextDateList.remove(position);
+                        adap.notifyDataSetChanged();
+                        alarm.cancelAlarm(getActivity(), position);
+
+                        Snackbar deleteMessageSnackBar = Snackbar.make(view, deletedName + " deleted successfully", Snackbar.LENGTH_SHORT);
+                        deleteMessageSnackBar.show();
+
+                    }
+                });
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
                 return true;
             }
         });
